4361b34ab8392bf7ee37c8a7c9d8a6e0c6401efb
geyan <2277087813@qq.com>
2022-11-06 00:27:11 +0800

    feat:【商品服务】done

--- productCatalogService/cmd/main.go
+++ productCatalogService/cmd/main.go
@@ -1,7 +1,52 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net"
+	"strconv"
+
+	"github.com/mars/productCatalogService/pb"
+	"github.com/mars/productCatalogService/service"
+	"google.golang.org/grpc"
+)
+
+const PORT = 60002
+const ADDRESS = "127.0.0.1"
+const SERVICE_NAME = "product_catalog_service"
 
 func main() {
-	fmt.Println("productCatalogService main...")
+	fmt.Println("productCatalogService start...")
+
+	// 1.1 创建consul
+	addr := ADDRESS + ":" + strconv.Itoa(PORT)
+	consul, err := service.NewConsul()
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	// 1.2 注册consul
+	err = consul.RegisterService(SERVICE_NAME, ADDRESS, PORT)
+	if err != nil {
+		fmt.Printf("consul服务注册失败: %v\n", err)
+		return
+	}
+
+	// 2.1 监控端口
+	listen, err := net.Listen("tcp", addr)
+	if err != nil {
+		fmt.Printf("监听端口失败: %v\n", err)
+		return
+	}
+	// 2.2 注册gRPC服务
+	grpcServer := grpc.NewServer()
+	productCatalogService := service.NewProductCatalogService()
+	pb.RegisterProductCatalogServiceServer(grpcServer, productCatalogService)
+
+	// 2.3 启动gRPC服务
+	fmt.Println("productCatalog service start success...")
+	err = grpcServer.Serve(listen)
+	if err != nil {
+		fmt.Printf("gRPC server start failed: %v\n", err)
+		return
+	}
 }
--- productCatalogService/service/productCatalog.go
+++ productCatalogService/service/productCatalog.go
@@ -3,13 +3,17 @@ package service
 import (
 	"context"
 	"encoding/json"
+	"fmt"
 	"io/ioutil"
+	"strings"
 	"sync"
 
 	"github.com/mars/productCatalogService/pb"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
 )
+
+var reloadCatalog bool
 
 type ProductCatalogService struct {
 	pb.UnimplementedProductCatalogServiceServer
@@ -25,6 +29,45 @@ func (service *ProductCatalogService) ListProducts(ctx context.Context, empty *p
 	out := new(pb.ListProductsResponse)
 	out.Products = service.parseCatalog()
 	return out, nil
+}
+
+func (service *ProductCatalogService) GetProduct(ctx context.Context, request *pb.GetProductRequest) (*pb.Product, error) {
+	id := request.GetId()
+
+	var found *pb.Product
+
+	out := &pb.Product{}
+
+	for _, product := range service.parseCatalog() {
+		if id == product.Id {
+			found = product
+			break
+		}
+	}
+	if found == nil {
+		return nil, status.Errorf(codes.NotFound, "找不到该商品")
+	}
+	// 不建议直接将found返回
+	out.Id = found.GetId()
+	out.Name = found.GetName()
+	out.Description = found.GetDescription()
+	out.Picture = found.GetPicture()
+	out.PriceUsd = found.GetPriceUsd()
+	out.Categories = found.GetCategories()
+
+	return out, nil
+}
+
+func (service *ProductCatalogService) SearchProducts(ctx context.Context, request *pb.SearchProductsRequest) (*pb.SearchProductsResponse, error) {
+	response := &pb.SearchProductsResponse{}
+	for _, product := range service.parseCatalog() {
+		if strings.Contains(strings.ToLower(request.GetQuery()), strings.ToLower(product.GetName())) ||
+			strings.Contains(strings.ToLower(request.GetQuery()), strings.ToLower(product.GetDescription())) {
+			// 命中
+			response.Results = append(response.Results, product)
+		}
+	}
+	return response, nil
 }
 
 // 读取配置文件
@@ -47,6 +90,19 @@ func (service *ProductCatalogService) readCatalogFile() (*pb.ListProductsRespons
 
 // 解析配置文件
 func (service *ProductCatalogService) parseCatalog() []*pb.Product {
+	if reloadCatalog || len(service.products) == 0 {
+		catalog, err := service.readCatalogFile()
+		if err != nil {
+			fmt.Printf("解析配置文件失败: %v\n", err)
+			return []*pb.Product{}
+		}
+		service.products = catalog.Products
+	}
+	return service.products
+}
+
+func init() {
+	// 简化
+	reloadCatalog = true
 	// TODO()
-	return nil
 }
